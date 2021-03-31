5eb606b025adf4c2237ac4c8c7810f99e6a1fd14
morobio@hugeinc.com <morobio@H9497.ad.hugeinc.com>
2021-03-30 20:37:55 -0500

    prospery dashboard build

---
+++ precache-manifest.522506b72998accc7d5d17498548d7a5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f4e47c5def4121ad40",
+    "url": "/css/app.6fbd5d51.css"
+  },
+  {
+    "revision": "33b2873eb3ab346f8d9b",
+    "url": "/css/chunk-vendors.12ee4af3.css"
+  },
+  {
+    "revision": "c9ce235700d9a5bcef34ef843c83eea8",
+    "url": "/fonts/Objective-Medium.c9ce2357.woff2"
+  },
+  {
+    "revision": "9f71f714eb9c53936d41a3ab39e32ad3",
+    "url": "/fonts/Objective-Regular.9f71f714.woff2"
+  },
+  {
+    "revision": "2381bc107a10ebc31214884ecea59794",
+    "url": "/img/done-status.json"
+  },
+  {
+    "revision": "619222e06f611d9b7c624c46a4585daa",
+    "url": "/img/id-img.619222e0.png"
+  },
+  {
+    "revision": "56b564880df58a4db42451c2dc197d20",
+    "url": "/img/selfie-img.56b56488.png"
+  },
+  {
+    "revision": "0f8178bddc9f9c1d8633c1ca336db1fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7f4e47c5def4121ad40",
+    "url": "/js/app.652cf031.js"
+  },
+  {
+    "revision": "33b2873eb3ab346f8d9b",
+    "url": "/js/chunk-vendors.6c7a9a57.js"
+  },
+  {
+    "revision": "da2c0fa5e63f40b233cd91c44e877c26",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
