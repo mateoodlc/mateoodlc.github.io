84d998b1b00a7ffc8a29cf624cd8f85baf9ed660
morobio@hugeinc.com <morobio@H9497.ad.hugeinc.com>
2021-06-17 15:15:36 -0500

    feat: testing uscell project

---
+++ precache-manifest.fdd2442d7043345ad06c78e7ebf10070.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2271e661c242248e173",
+    "url": "/app/css/app.526aff17.css"
+  },
+  {
+    "revision": "fa11c3ca7b2301d6b8da51b04985fb05",
+    "url": "/app/fonts/BrandonGrotesque-Bold.fa11c3ca.otf"
+  },
+  {
+    "revision": "ec269707ba6ac99c9fb336d8ee943b0d",
+    "url": "/app/img/header-logos.ec269707.png"
+  },
+  {
+    "revision": "d44cbec0048d00d12a27e5a1b25c3121",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "b2271e661c242248e173",
+    "url": "/app/js/app.3cf792d3.js"
+  },
+  {
+    "revision": "a5842827b46858d610d2",
+    "url": "/app/js/chunk-vendors.2a1e5f56.js"
+  },
+  {
+    "revision": "2f583cabad6c530167b39a4d5de85197",
+    "url": "/app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/app/robots.txt"
+  }
+]);
